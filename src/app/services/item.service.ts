e4710cde2c2247b5a322f175bd8ea12f71151b5c
Pugsworth <pworthwellington@gmail.com>
2022-03-14 12:47:38 -0600

    ItemService now controls filtering and selecting.

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,17 +1,22 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { ItemSchema } from '../itemschema';
 import { HttpClient } from "@angular/common/http";
+import { DisplayItem } from '../DisplayItem';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
-export class ItemService {
+export class ItemService 
+{
+    // item related events
+    filter = new EventEmitter<string>();
+    itemSelected = new EventEmitter<DisplayItem>();
 
-  constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient) { }
 
-  getItems(): Observable<ItemSchema[]>
-  {
-    return this.http.get<ItemSchema[]>("./assets/data/allitemsdata.json");
-  }
+	getItems(): Observable<ItemSchema[]>
+	{
+		return this.http.get<ItemSchema[]>("./assets/data/allitemsdata.json");
+	}
 }
